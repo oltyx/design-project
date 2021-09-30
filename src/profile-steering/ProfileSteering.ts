a7bd710905c697658c4757acc186e3d8e73b9ecd
Ivo_Broekhof <i.broekhof@student.utwente.nl>
2021-09-30 18:23:08 +0200

    Finished initialization of the algo

--- src/profile-steering/ProfileSteering.ts
+++ src/profile-steering/ProfileSteering.ts
@@ -6,8 +6,28 @@
  * @param powerlimitsUpper  How much energy the charger is able to take per time interval, empty by default.
  * @param prices            The price signals per time interval, or all zeroes by default.
  * @param beta              The rate of punishment, as a scalar for the cost. 1 by default.
+ * @return result           The optimal profile it found.
  */
 function discreteBufferPlanningPositive(desired: number[][], chargeRequired: number, chargingPowers: number[],
-                                         powerlimitsUpper: number[] = [], prices: number[] = null, beta: number = 1) {
-    console.error("Not yet implemented"); //TODO implement
+                                         powerlimitsUpper: number[] = [], prices: number[] = null, beta: number = 1):number[] {
+    // Initialization of the algorithm
+    let result:number[] = new Array(desired.length);
+    for (let i = 0; i < result.length; i++) {result[i] = 0;}
+
+    let remainingCharge: number = chargeRequired;
+
+    if (prices == null) {
+        prices = new Array(desired.length);
+        for (let i = 0; i < prices.length; i++) {prices[i] = 0;}
+    }
+
+    chargingPowers.sort();
+    console.assert(chargingPowers.length >= 1);
+
+    let slopes: number[] = [];
+
+    // Body of the algorithm
+    console.error("Not (fully) implemented yet"); //TODO implement
+
+    return result;
 }
