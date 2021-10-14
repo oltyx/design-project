6d3018afc608c71d4295002d0821497313aebae4
Alex <a.olteanu@student.utwente.nl>
2021-10-14 21:13:25 +0200

    Fix image and font scaling

--- src/components/pages/Start.tsx
+++ src/components/pages/Start.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { useHistory, withRouter  } from "react-router-dom";
+import { Button, Col, Container, Row } from 'reactstrap';
+import EvCar from '../../assets/ev_car.svg';
+import '../../styles/lightMode.scss';
+import '../../styles/start.scss';
 
 // Landing page
 export default function Start() {
@@ -7,11 +11,35 @@ export default function Start() {
   let history = useHistory();
 
   function handleClick() {
-    history.push("/feedback");
+    history.push("/schedule");
   }
     return(
-        <div>
-            <button onClick={handleClick}>Start</button>
-        </div>
+        <Container className={"startPage"} fluid={true}>
+
+            <Row >
+                <Col>
+                    <h1>
+                        Hello, Jos!
+                    </h1>
+                </Col>
+            </Row>
+                    <img className={"carStyle"} src={EvCar} alt="Generic car" />
+            <Row>
+                <Col>
+                    <h4>
+                        Ready?
+                    </h4>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <Button className={"globalButton"} onClick={() => handleClick()}>
+                        Set
+                    </Button>
+                </Col>
+            </Row>
+
+        </Container>
     )
 }
