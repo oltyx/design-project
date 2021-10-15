4f2c99cb2bd875d9e6c45638b69d392d8179998c
ivobroekhof <i.broekhof@student.utwente.nl>
2021-10-15 17:04:38 +0200

    Stub version of Schedule page

--- src/components/pages/Schedule.tsx
+++ src/components/pages/Schedule.tsx
@@ -4,10 +4,20 @@ import '../widgets/TimeSelector';
 import '../widgets/EnergySelector';
 import '../widgets/ModeSelector';
 import ModeSelector from "../widgets/ModeSelector";
+import TimeSelector from "../widgets/TimeSelector";
+import EnergySelector from "../widgets/EnergySelector";
+import Graph from '../widgets/Graph';
 
 // Scheduling page
 export default function Schedule() {
     return(<body>
+        Select Departure Time
+        <TimeSelector/>
+        Select Energy Consumption
+        <EnergySelector/>
+        Select Charging Mode
         <ModeSelector/>
+        Charging Schedule
+        <Graph/>
     </body>)
 }
