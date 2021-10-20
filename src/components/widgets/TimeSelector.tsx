1bac318a8a44eff96bad422ad4d32900bdea0586
Alex <a.olteanu@student.utwente.nl>
2021-10-20 15:09:32 +0200

    Initial skeleton

--- src/components/widgets/TimeSelector.tsx
+++ src/components/widgets/TimeSelector.tsx
@@ -3,13 +3,28 @@ import {Box} from "@mui/material";
 
 type Time = {hours: number, minutes: number};
 
-const DEFAULT_TIME : Time = {hours: 17, minutes: 0};
+const DEFAULT_TIME : Time = {hours: 17, minutes: 30};
 
 export default function TimeSelector() {
     const [{hours, minutes}, setHoursMinutes] = useState<Time>(DEFAULT_TIME);
 
-    return (<Box sx={{ width: 200 }}>
-        {hours} : {minutes}
-    </Box>
+    return (
+
+        <div className="time-picker" data-time="00:00">
+            <div className="hour">
+                <div className="hr-up"></div>
+                    <input disabled type="number" className="hr" value={hours}/>
+                <div className="hr-down"></div>
+            </div>
+
+            <div className="separator">:</div>
+
+            <div className="minute">
+                <div className="min-up"></div>
+                    <input disabled type="number" className="min" value={minutes}/>
+                <div className="min-down"></div>
+            </div>
+        </div>
+
     );
 }
