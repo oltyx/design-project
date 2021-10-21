f11fd1e48574542c9cfbe1d41a3342b2b9339523
Alex <a.olteanu@student.utwente.nl>
2021-10-21 12:58:15 +0200

    Functional TimeSelector component

--- src/components/widgets/TimeSelector.tsx
+++ src/components/widgets/TimeSelector.tsx
@@ -1,30 +1,94 @@
-import React, {useState} from 'react';
-import {Box} from "@mui/material";
+import React, {MutableRefObject, useEffect, useRef} from 'react';
 
-type Time = {hours: number, minutes: number};
+import '../../styles/timeSelector.scss';
 
-const DEFAULT_TIME : Time = {hours: 17, minutes: 30};
+export interface TimeSelectorProps {
+    hour: number,
+    setHour: (hour: number) => void,
+    minutes: number,
+    setMinutes: (hour: number) => void
+}
 
-export default function TimeSelector() {
-    const [{hours, minutes}, setHoursMinutes] = useState<Time>(DEFAULT_TIME);
+const TimeSelector: React.FC<TimeSelectorProps> = ({ hour, setHour, minutes, setMinutes}) => {
+
+    const hrUpRef = useRef() as MutableRefObject<HTMLDivElement>;
+    const hrDownRef = useRef() as MutableRefObject<HTMLDivElement>;
+    const minUpRef = useRef() as MutableRefObject<HTMLDivElement>;
+    const minDownRef = useRef() as MutableRefObject<HTMLDivElement>;
+    const hourInputRef = useRef() as MutableRefObject<HTMLInputElement>;
+    const minutesInputRef = useRef() as MutableRefObject<HTMLInputElement>;
+
+    const hourUp = () => {
+        setHour((((hour + 1) % 24) + 24) % 24);
+    };
+
+    const hourDown = () => {
+      setHour((((hour - 1) % 24) + 24) % 24);
+    };
+
+    const minutesUp = () => {
+      setMinutes((((minutes + 15) % 60) + 60) % 60);
+    };
+
+    const minutesDown = () => {
+        setMinutes((((minutes - 15) % 60) + 60) % 60);
+    };
+
+    useEffect(() => {
+        hrUpRef.current.addEventListener('click', hourUp);
+        hrDownRef.current.addEventListener('click', hourDown);
+        minUpRef.current.addEventListener('click', minutesUp);
+        minDownRef.current.addEventListener('click', minutesDown);
+        return () => {
+            hrUpRef.current.removeEventListener('click', hourUp)
+            hrDownRef.current.removeEventListener('click', hourDown);
+            minUpRef.current.removeEventListener('click', minutesUp);
+            minDownRef.current.removeEventListener('click', minutesDown);
+        };
+    });
+
+    useEffect(() => {
+        hourInputRef.current.value = formatTime(hour);
+    },[hour])
+
+    useEffect(() => {
+        minutesInputRef.current.value = formatTime(minutes)
+    }, [minutes])
+
+    const formatTime = (time: number): string => {
+        if(time < 10){
+            return '0' + time.toString();
+        }
+        return time.toString();
+    };
 
     return (
 
-        <div className="time-picker" data-time="00:00">
+        <div className="time-picker">
             <div className="hour">
-                <div className="hr-up"></div>
-                    <input disabled type="number" className="hr" value={hours}/>
-                <div className="hr-down"></div>
+                <div ref={hrUpRef} className="hr-up"/>
+                    <input disabled
+                           ref={hourInputRef}
+                           type="number"
+                           className="hr"
+                           value={hour}/>
+                <div ref={hrDownRef} className="hr-down"/>
             </div>
 
             <div className="separator">:</div>
 
             <div className="minute">
-                <div className="min-up"></div>
-                    <input disabled type="number" className="min" value={minutes}/>
-                <div className="min-down"></div>
+                <div ref={minUpRef} className="min-up"/>
+                    <input disabled
+                           ref={minutesInputRef}
+                           type="number"
+                           className="min"
+                           value={minutes}/>
+                <div ref={minDownRef} className="min-down"/>
             </div>
         </div>
 
     );
 }
+
+export default TimeSelector;
