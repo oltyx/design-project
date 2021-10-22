58773b794f1f829195a264ff1a9c54f9a6d747c4
allahverdiyevaa <a.allahverdiyeva@student.utwente.nl>
2021-10-22 12:37:21 +0200

    removed timeselector (shit breaks), kinda fixed buttons and alert

--- src/components/widgets/EnergySelector.tsx
+++ src/components/widgets/EnergySelector.tsx
@@ -1,21 +1,22 @@
 import React, {useState} from 'react';
-import Slider from '@mui/material/Slider';
+//import Slider from '@mui/material/Slider';
 
 import '../../assets/profile-steering/Conversion';
 import {WhToKm} from "../../assets/profile-steering/Conversion";
-import {Box} from "@mui/material";
+//import {Box} from "@mui/material";
 
 interface Energy {energy: number, setEnergy: (newValue: number) => void};
 
 // Slider for km and kWh, with conversion from Conversion.ts
 export default function EnergySelector({energy, setEnergy}:Energy) {
+    return(<></>)
 
-    return(<Box sx={{ width: 200 }}>
-        <Slider defaultValue={energy} step={500} min={0} max={100000} onChange={
-            (event: Event, newValue: number | number[]) => {
-            setEnergy(newValue as number);
-            }
-        }/>
-        {energy / 1000} kWh = {Math.round(WhToKm(energy))} km
-    </Box>);
+    // return(<Box sx={{ width: 200 }}>
+    //     <Slider defaultValue={energy} step={500} min={0} max={100000} onChange={
+    //         (event: Event, newValue: number | number[]) => {
+    //         setEnergy(newValue as number);
+    //         }
+    //     }/>
+    //     {energy / 1000} kWh = {Math.round(WhToKm(energy))} km
+    // </Box>);
 }
