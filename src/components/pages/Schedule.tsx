1193d31af1d67ea0a3b5437fa6fa4414532d4b0a
s2189496 <a.iosif@student.utwente.nl>
2021-10-22 12:38:32 +0200

    better design

--- src/components/pages/Schedule.tsx
+++ src/components/pages/Schedule.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { Container, Row, Col} from 'reactstrap';
 
 import '../widgets/TimeSelector';
 import '../widgets/EnergySelector';
@@ -10,6 +11,7 @@ import Graph from '../widgets/Graph';
 import {ChargingMode} from "../../data/models/ChargingMode";
 import {GlobalButton} from "../styled/Button";
 import {useHistory} from "react-router-dom";
+import '../../styles/schedule.scss';
 
 const DEFAULT_TIME = {hour: 17, minutes: 30};
 const DEFAULT_CHARGE: number = 0;
@@ -28,15 +30,37 @@ export default function Schedule() {
     const [energy, setEnergy] = useState<number>(DEFAULT_CHARGE);
     const [mode, setMode] = useState<ChargingMode>(DEFAULT_MODE);
 
-    return(<body>
-        Select Departure Time
-        <TimeSelector hour={hour} setHour={setHour} minutes={minutes} setMinutes={setMinutes} />
-        Select Energy Consumption
-        <EnergySelector energy={energy} setEnergy={setEnergy}/>
-        Select Charging Mode
-        <ModeSelector mode={mode} setMode={setMode}/>
-        Charging Schedule
-        <Graph chargeRequired={energy} endHr={hour} endMin={minutes} mode={mode}/>
-        <GlobalButton text={"Go"} onClick={handleClick}/>
-    </body>)
-}
+    return(
+        <Container className="schedule">
+            <Row>
+                <Col>
+                    Select Departure Time
+                    <TimeSelector hour={hour} setHour={setHour} minutes={minutes} setMinutes={setMinutes} />
+                </Col>
+            </Row>
+            <Row className="energyRow">
+                <Col>
+                    Select Energy Consumption
+                    <EnergySelector energy={energy} setEnergy={setEnergy}/>
+                </Col>
+            </Row>
+            <Row className="modeRow">
+                <Col>
+                    Select Charging Mode
+                    <ModeSelector mode={mode} setMode={setMode}/>
+                </Col>
+            </Row>
+            <Row className="graphRow">
+                <Col>
+                    Charging Schedule
+                    <Graph chargeRequired={energy} endHr={hour} endMin={minutes} mode={mode}/>
+                </Col>
+            </Row>
+            <Row className="goButtonRow">
+                <Col>
+                    <GlobalButton text={"Go"} onClick={handleClick}/>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
