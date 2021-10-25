8793b5e4f1ea157cc480ef8df9be62c2759ddd83
s2189496 <a.iosif@student.utwente.nl>
2021-10-25 12:28:57 +0200

    pop up done except color

--- src/components/pages/ChargingSession.tsx
+++ src/components/pages/ChargingSession.tsx
@@ -1,6 +1,6 @@
 import React, {useCallback, useState} from 'react';
 import { useHistory } from "react-router-dom";
-import { Container, Row, Col, Label} from 'reactstrap';
+import { Container, Row, Col, Label, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 import EvCar from '../../assets/ev_car.svg';
 import { GlobalButton } from '../styled/Button';
@@ -8,14 +8,24 @@ import Stats from '../widgets/Stats';
 import ProgressBar from '../widgets/ProgressBar';
 import '../../styles/chargingSession.scss';
 
-
+      
 // Page for the charging session as it goes on ("Session Page" on Figma)
 export default function ChargingSession() {
+
+    const [modal, setModal] = useState(false);
+    const handleYes = () => {
+        setModal(!modal);
+        setTime(today.getHours() + ' : ' + today.getMinutes());
+        setSessionTitle("Aborted at ");
+        setButton("Finish");
+        setStyle("linear-gradient(to bottom, #F07878 0%, #9D1616 100%)");
+    }
+    const handleCancel = () => setModal(!modal);
 
     const [sessionTitle, setSessionTitle] = useState("Departure at ");
     const [button, setButton] = useState("Stop");
     const [time, setTime] = useState("13 : 15");
-    const [style, setStyle] = useState("linear-gradient(to bottom, #9AE09A 0%, #44BE44 100%)")
+    const [style, setStyle] = useState("linear-gradient(to bottom, #9AE09A 0%, #44BE44 100%)");
 
     const today = new Date();
 
@@ -23,45 +33,56 @@ export default function ChargingSession() {
 
     const handleClick = useCallback(() => {
         if (button==="Stop") {
-            setTime(today.getHours() + ' : ' + today.getMinutes());
-            setSessionTitle("Aborted at ");
-            setButton("Finish");
-            setStyle("linear-gradient(to bottom, #F07878 0%, #9D1616 100%)")
+            setModal(true);
         } else {
             history.push("/feedback");
         }
     }, [history, style, sessionTitle, button, time]);
 
     return(
-    <Container className="chargingSession">
-        <Row>
-            <Col>
-                <Label className="chargingTitle">{sessionTitle} {time}</Label>
-            </Col>
-        </Row>
-        <Row className={"carRow"}>
-            <Col>
-                <img className={"carStyle"}
-                    src={EvCar}
-                    alt="Generic car"
-                />
-            </Col>
-        </Row>
-        <Row className={"w-100"}>
-            <Col>
-                <ProgressBar style={style}/>
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-                <Stats/>
-            </Col>
-        </Row>
-        <Row className="buttonRow"> 
-            <Col>
-                <GlobalButton text={button} onClick={handleClick}/>
-            </Col>
-        </Row>
-    </Container>
+        <Container className="chargingSession">
+            <Row>
+                <Col>
+                    <Label className="chargingTitle">{sessionTitle} {time}</Label>
+                </Col>
+            </Row>
+            <Row className={"carRow"}>
+                <Col>
+                    <img className={"carStyle"}
+                        src={EvCar}
+                        alt="Generic car"
+                    />
+                </Col>
+            </Row>
+            <Row className={"w-100"}>
+                <Col>
+                    <ProgressBar style={style}/>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Stats/>
+                </Col>
+            </Row>
+            <Row className="buttonRow"> 
+                <Col>
+                    <GlobalButton text={button} onClick={handleClick}/>
+                </Col>
+            </Row>
+            <Row> 
+                <Col >
+                    <Modal className="popUp" isOpen={modal}  >
+                        <ModalHeader >Confirmation</ModalHeader>
+                        <ModalBody>
+                            Are you sure you want to abort the charging session?
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button className="yesButton" onClick={handleYes}>Yes</Button>
+                            <Button className="cancelButton" onClick={handleCancel}>Cancel</Button>
+                        </ModalFooter>
+                    </Modal>
+                </Col>
+            </Row>
+        </Container>
     );
 };
