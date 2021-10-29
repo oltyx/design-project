735bebb1c1d1ce4c577a702a65219da2abdcc9ea
ivobroekhof <i.broekhof@student.utwente.nl>
2021-10-29 10:59:22 +0200

    Fixed unit oopsie daisy

--- src/assets/profile-steering/PriceEmissions.ts
+++ src/assets/profile-steering/PriceEmissions.ts
@@ -4,25 +4,25 @@ import {ChargingData} from "../../data/models/ChargingData";
  * Average CO2 emissions of grid energy, in g per Wh.
  * Source: https://nl.econologie.com/europe-emissie-co2-country-kwh-elektriciteit/
  */
-const CO2_EMISSIONS_GRID : number = 0.642;
+const CO2_EMISSIONS_GRID : number = 642;
 
 /**
  * Average CO2 emissions of solar energy, in g per Wh.
  * Source: https://www.treehugger.com/how-much-co-does-one-solar-panel-create-4868753
  */
-const CO2_EMISSIONS_SOLAR : number = 0.050;
+const CO2_EMISSIONS_SOLAR : number = 50;
 
 /**
- * Average price of grid energy, in €/W.
+ * Average price of grid energy, in €/kW.
  * Source: https://www.consumentenbond.nl/energie-vergelijken/kwh-prijs
  */
-const PRICE_GRID : number = 0.00024;
+const PRICE_GRID : number = 0.24;
 
 /**
- * Price of solar energy, in €/W.
+ * Price of solar energy, in €/kW.
  * This is half the price of grid energy for now.
  */
-const PRICE_SOLAR : number = 0.00012;
+const PRICE_SOLAR : number = 0.12;
 
 export function getPrice(data: ChargingData[]): number {
     let res: number = 0;
