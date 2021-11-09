e0ca6a1a08481a4ef90016c4863f2cab83ac17af
s2189496 <a.iosif@student.utwente.nl>
2021-11-09 16:29:55 +0100

    comments session page, progress bar and stats components

--- src/components/widgets/ProgressBar.tsx
+++ src/components/widgets/ProgressBar.tsx
@@ -5,14 +5,26 @@ import { Progress, Container, Row, Col } from 'reactstrap';
 import '../../styles/lightMode.scss';
 import '../../styles/chargingSession.scss';
 
-
+/**
+ * Types for the ProgressBar Component Props.
+ */
 interface ProgressBarProps {
+
+    /**
+     * Type of string for the progress bar style.
+     */
     style?: string,
+
+    /**
+     * Type of number of the progress bar value.
+     */
     value: number
 }
-// Progress bar for the charging
 export default function ProgressBar({value, ...props}: ProgressBarProps) {
-
+   
+    /**
+     * Default progress bar style.
+     */
     const iconStyle = { backgroundImage: "linear-gradient(to bottom, #9AE09A 0%, #44BE44 100%)", width: "100%", borderColor: "black" }
 
     return(
